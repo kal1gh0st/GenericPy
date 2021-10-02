9c53c08fb7afdebd96d033946c920d0fa5266cdc
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-10-03 01:06:29 +0200

    Update compilatutto.py

--- compilatutto.py
+++ compilatutto.py
@@ -1,5 +1,5 @@
 function compilatutto(value) {
-  if (value == 'carne') {
+  if (value == 'pussy') {
   document.getElementById('prezzo1').value="15.00";
   document.getElementById('img_prod1').src="http://forum.mrwebmaster.it/images/prodotti/mini/00.png";
   document.getElementById('link1').href="http://forum.mrwebmaster.it/images/prodotti/00.png";
